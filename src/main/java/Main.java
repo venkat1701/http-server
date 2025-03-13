e0c0cbca5a512c4e50573a367850e64d93de0067
Venkat <thejeastdev@gmail.com>
2025-03-14 00:25:42 +0530

    binding-a-http-server-to-port

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,19 +7,16 @@ public class Main {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
-    // try {
-    //   ServerSocket serverSocket = new ServerSocket(4221);
-    //
-    //   // Since the tester restarts your program quite often, setting SO_REUSEADDR
-    //   // ensures that we don't run into 'Address already in use' errors
-    //   serverSocket.setReuseAddress(true);
-    //
-    //   serverSocket.accept(); // Wait for connection from client.
-    //   System.out.println("accepted new connection");
-    // } catch (IOException e) {
-    //   System.out.println("IOException: " + e.getMessage());
-    // }
+     try {
+       ServerSocket serverSocket = new ServerSocket(4221);
+
+       // Makes sure that we dont get any issues about busy ports.
+       serverSocket.setReuseAddress(true);
+
+       serverSocket.accept(); // Wait for connection from client. Blocking method since it waits for a connection.
+       System.out.println("accepted new connection");
+     } catch (IOException e) {
+       System.out.println("IOException: " + e.getMessage());
+     }
   }
 }
