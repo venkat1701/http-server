aa85317e5f625c01fefc47f63d408d7ab4f758bb
Venkat <thejeastdev@gmail.com>
2025-03-19 02:47:49 +0530

    http-server main file docs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,11 +7,14 @@ public class Main {
 
     System.out.println("Logs from your program will appear here!");
 
+    // Try with resources - Java 7 implements AutoCloseable on I/O streams.
      try(ServerSocket serverSocket = new ServerSocket(4221)) {
        serverSocket.setReuseAddress(true);
 
+       // Accepting connections infinitely.
        while(true) {
            Socket socket = serverSocket.accept();
+           // Need a logger here, will add that soon.
            System.out.println("New connection from " + socket.getRemoteSocketAddress());
            new Thread(new SocketHandler(socket)).start();
        }
