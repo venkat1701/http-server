7a2bef8b4105aa1bc9ed09f6ae7f2f0c05592a34
Venkat <thejeastdev@gmail.com>
2025-03-19 03:01:03 +0530

    http-server socket-handler

--- src/main/java/SocketHandler.java
+++ src/main/java/SocketHandler.java
@@ -4,6 +4,17 @@ import java.io.OutputStream;
 import java.net.Socket;
 import java.util.logging.Logger;
 
+/**
+ * SocketHandler brings out each client connection on a separate kernel thread. Inside of their run method,
+ * we parse the request using a URLParser to get the parts of the Http Request.
+ * For instance, let's say we type this command in terminal <pre>curl -v http://localhost:4221/abcd</pre>
+ * Every HTTP Request is made up of 3 parts: Request line, zero or more headers ending with CRLF and an optional request body
+ * <pre>GET /index.html HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.64.1\r\nAccept: CRLF</pre>
+ * The request target specifies the URL path for this request. Here the URL path is <strong>/index.html</strong>
+ * This parsing is done by URL Parser.
+ * @see URLParser
+ * @author Venkat
+ */
 public class SocketHandler implements Runnable{
     private static final Logger logger = Logger.getLogger("SocketHandler");
     private final Socket client;
